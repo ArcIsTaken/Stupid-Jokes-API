84735b21f27fcd9db1098a012f89634f9c161bf1
Riley <riley.freier@gmail.com>
2023-09-20 21:16:58 -0600

    added some more css, finished up the safemode button, and attempted to import the .js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("page ready");
+  const storedSwitchState = localStorage.getItem("switchState");
+  const switchInput = document.getElementById("switch");
+
+  if (storedSwitchState) {
+    // Retrieve the stored value and set the checkbox state accordingly
+    switchInput.checked = JSON.parse(storedSwitchState);
+  }
+
+  // Add an event listener to the checkbox to update the stored value
+  switchInput.addEventListener("change", function () {
+    localStorage.setItem("switchState", switchInput.checked);
   });
+});
+
